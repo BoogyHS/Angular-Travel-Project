c2dbaf2f028e0d74cbcc9b62aba7b7de5839b8ad
Boogy83 <bozhidar.atanasov@abv.bg>
2020-02-05 17:02:24 +0200

    services working with kinvey

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 //Modules
 import { CoreModule } from './core/core.module';
 import { UserModule } from './user/user.module';
+
+//Services
+import { CountriesService } from './services/countries.service';
 
 //Components
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CountryComponent } from './country/country.component';
+import { UsersService } from './services/users.service';
 
 @NgModule({
   declarations: [
@@ -18,14 +24,16 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     HomeComponent,
     AboutComponent,
     DashboardComponent,
+    CountryComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CoreModule,
-    UserModule
+    UserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [CountriesService, UsersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CountriesService } from '../services/countries.service';
+import { ICountry } from '../models/country';
+import { UsersService } from '../services/users.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  countries: ICountry
 
-  constructor() { }
+  constructor(private userService: UsersService, private countriesService: CountriesService) { }
 
   ngOnInit() {
+    this.userService
+      .getLogin()
+      .subscribe(data => {
+        // this.countries = data;
+        console.log(data);
+      });
+    this.countriesService
+      .getCountries()
+      .subscribe(data => {
+        // this.countries = data;
+        console.log(data);
+      });
   }
 
 }
