193c0207da085dc05da38e0642a71d550e33544c
Boogy83 <bozhidar.atanasov@abv.bg>
2020-02-18 14:27:48 +0200

    select menu with country and city implemented

--- src/app/add-travel/add-travel.component.ts
+++ src/app/add-travel/add-travel.component.ts
@@ -1,20 +1,21 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { DataStoreService, Query } from 'kinvey-angular-sdk';
-import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-add-travel',
   templateUrl: './add-travel.component.html',
   styleUrls: ['./add-travel.component.scss']
 })
-export class AddTravelComponent implements OnInit {
-
-  @ViewChild('selectedCountry', { static: false }) selectedCountry: ElementRef;
+export class AddTravelComponent implements OnInit, OnDestroy {
 
   form: FormGroup;
   allCountries: any;
+  allCities: any;
   countries: any;
+  cities: any;
+  selectedCountry: string;
+  selectedCity: any;
 
   constructor(
     private fb: FormBuilder,
@@ -22,25 +23,51 @@ export class AddTravelComponent implements OnInit {
   ) {
     this.form = this.fb.group({
       countrySelect: ['', [Validators.required]],
+      citySelect: ['', [Validators.required]],
       text: ['', [Validators.required]],
     });
 
     this.allCountries = datastoreService.collection('countriesArr');
+    this.allCities = datastoreService.collection('citiesArr');
   }
 
   ngOnInit() {
+
     const query = new Query();
     query.ascending('country');
     query.fields = ['country'];
     this.allCountries.find(query)
       .subscribe((countries) => {
         this.countries = countries;
-        console.log(countries);
+        // console.log(countries);
 
       }, (error) => {
         console.log(error);
       });
   }
 
+  changeSelectedCountry() {
+    this.selectedCountry = this.form.get('countrySelect').value;
 
+    const query = new Query();
+    query.ascending('citi_ascii');
+    query.equalTo('country', this.selectedCountry);
+    this.allCities.find(query)
+    .subscribe((cities) => {
+      this.cities = cities;
+      console.log(cities);
+
+    }, (error) => {
+      console.log(error);
+    });
+  }
+
+  changeSelectedCity($event){
+    this.selectedCity = this.form.get('citySelect').value;
+    console.log(this.selectedCity);
+
+  }
+
+  ngOnDestroy() {
+  }
 }
